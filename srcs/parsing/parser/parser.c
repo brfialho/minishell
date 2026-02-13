e5d32ff277bcc9fb1ac5a77b5d65593fce85a308
Bruno <brunolegal3@gmail.com>
2026-02-12 22:25:12 -0300

    libft update // moved generic function lst_cut to libft

--- srcs/parsing/parser/parser.c
+++ srcs/parsing/parser/parser.c
@@ -6,35 +6,13 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 17:10:05 by brfialho          #+#    #+#             */
-/*   Updated: 2026/02/12 22:17:53 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/02/12 22:21:24 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-t_list	*lst_cut(t_list **head, t_list *node)
-{
-	t_list	*lst;
 
-	if (*head == node)
-	{
-		*head = (*head)->next;
-		node->next = NULL;
-		return (node);
-	}
-	lst = *head;
-	while (lst)
-	{
-		if (lst->next == node)
-		{
-			lst->next = NULL;
-			node->next = NULL;
-			return (node);
-		}
-		lst = lst->next;
-	}
-	return (NULL);
-}
 
 t_list	*get_lower_prec(t_list *token_lst)
 {
