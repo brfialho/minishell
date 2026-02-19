9ae3a1a1992f67feab550694db7d86a4c7265343
brfialho <brunolegal3@gmail.com>
2026-02-19 18:52:03 -0300

    .

--- srcs/execution/execution_test_main.c
+++ srcs/execution/execution_test_main.c
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 18:39:22 by brfialho          #+#    #+#             */
-/*   Updated: 2026/02/19 18:42:12 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/02/19 18:51:11 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,22 @@
 
 int g_status_shell = 0;
 
+void check_node_type(t_msh_ast *content)
+{
+	if (content->type == NODE_EXEC)
+		ft_printf("EXEC\n");
+}
+
 int main(int argc, char **argv, char **env)
 {
+	t_mini mini;
+
 	ft_printf("Main de teste para exec!\n");
+	ft_lexer(&mini.lexer, "|");
+	mini.root = parser(&mini.lexer);
+
+	t_msh_ast *content = (*mini.root)->content;
+	check_node_type(content);
 	(void)argc;
 	(void)argv;
 	(void)env;
