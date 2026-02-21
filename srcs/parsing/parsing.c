c22c3aa5fdd368930ba2fd03a0a02b8d07f10916
brfialho <brunolegal3@gmail.com>
2026-02-21 00:13:15 -0300

    feat: trim_quoted_tokens

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,14 +6,46 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 18:22:05 by brfialho          #+#    #+#             */
-/*   Updated: 2026/02/19 18:30:57 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/02/21 00:08:00 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
+void	trim_quotes(t_token *token)
+{
+	char	*new;
+	int		len;
+
+	len = ft_strlen(token->string);
+	if (len == 2)
+	{
+		free(token->string);
+		token->string = safe_calloc(1, sizeof(char));
+		return ;
+	}
+	new = safe_calloc(len - 1, sizeof(char));
+	ft_memcpy(new, token->string + 1, len - 2);
+	free(token->string);
+	token->string = new;
+}
+
+void	trim_quoted_tokens(t_lexer *lexer)
+{
+	t_list	*lst;
+
+	lst = *lexer->token_lst;
+	while (lst)
+	{
+		if (ft_str_charcount("\"'", ((t_token *)lst->content)->string[0]))
+			trim_quotes(lst->content);
+		lst = lst->next;
+	}
+}
+
 void	parsing(t_mini *mini)
 {
 	ft_lexer(&mini->lexer, mini->input);
+	trim_quoted_tokens(&mini->lexer);
 	mini->root = parser(&mini->lexer);
 }
