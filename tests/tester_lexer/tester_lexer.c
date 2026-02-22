46633a96a14f358d88dab12685cc3c73736978d1
brfialho <brunolegal3@gmail.com>
2026-02-22 00:29:54 -0300

    test: added extra test to lexer_tester

--- tests/tester_lexer/tester_lexer.c
+++ tests/tester_lexer/tester_lexer.c
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 17:39:21 by brfialho          #+#    #+#             */
-/*   Updated: 2026/02/21 20:19:26 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/02/22 00:29:00 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,9 +35,9 @@ char	test_lexer_simple_input(t_lexer *lexer)
 	if (!lst || lst_size(lst) != 4)
 		return (EXIT_FAILURE);
 
-	expected_token[0] = (t_token){WORD, "ls", 0, TRUE, FALSE};
+	expected_token[0] = (t_token){WORD, "ls", 0, TRUE, TRUE};
 	expected_token[1] = (t_token){PIPE, "|", 1, FALSE, FALSE};
-	expected_token[2] = (t_token){WORD, "wc", 0, TRUE, FALSE};
+	expected_token[2] = (t_token){WORD, "wc", 0, TRUE, TRUE};
 	expected_token[3] = (t_token){WORD, "-l", 0, TRUE, FALSE};
 
 	int i = -1;
@@ -87,20 +87,20 @@ char	test_lexer_complex_input(t_lexer *lexer)
 		return (EXIT_FAILURE);
 
 	// "   echo \"Hello world $teste \" | wc -l > outfile.txt && cat outfile.txt && rm -rf outfile.txt";
-	expected_token[0] = (t_token){WORD, "echo", 0, TRUE, FALSE};
+	expected_token[0] = (t_token){WORD, "echo", 0, TRUE, TRUE};
 	expected_token[1] = (t_token){WORD, "\"Hello world $teste \"", 0, TRUE, TRUE};
 	expected_token[2] = (t_token){PIPE, "|", 1, FALSE, FALSE};
-	expected_token[3] = (t_token){WORD, "wc", 0, TRUE, FALSE};
-	expected_token[4] = (t_token){WORD, "-l", 0, TRUE, FALSE};
+	expected_token[3] = (t_token){WORD, "wc", 0, TRUE, TRUE};
+	expected_token[4] = (t_token){WORD, "-l", 0, TRUE, TRUE};
 	expected_token[5] = (t_token){OUTFILE, ">", 0, FALSE, FALSE};
-	expected_token[6] = (t_token){WORD, "outfile.txt", 0, TRUE, FALSE};
+	expected_token[6] = (t_token){WORD, "outfile.txt", 0, TRUE, TRUE};
 	expected_token[7] = (t_token){LOGICAL_AND, "&&", 2, FALSE, FALSE};
-	expected_token[8] = (t_token){WORD, "cat", 0, TRUE, FALSE};
-	expected_token[9] = (t_token){WORD, "outfile.txt", 0, TRUE, FALSE};
+	expected_token[8] = (t_token){WORD, "cat", 0, TRUE, TRUE};
+	expected_token[9] = (t_token){WORD, "outfile.txt", 0, TRUE, TRUE};
 	expected_token[10] = (t_token){LOGICAL_AND, "&&", 2, FALSE, FALSE};
-	expected_token[11] = (t_token){WORD, "rm", 0, TRUE, FALSE};
-	expected_token[12] = (t_token){WORD, "-rf", 0, TRUE, FALSE};
-	expected_token[13] = (t_token){WORD, "outfile.txt", 0, TRUE, FALSE};
+	expected_token[11] = (t_token){WORD, "rm", 0, TRUE, TRUE};
+	expected_token[12] = (t_token){WORD, "-rf", 0, TRUE, TRUE};
+	expected_token[13] = (t_token){WORD, "outfile.txt", 0, TRUE, TRUE};
 
 	int i = -1;
 	while (++i < lst_size(lst))
@@ -123,15 +123,15 @@ char	test_lexer_complex_with_exports(t_lexer *lexer)
 	if (!lst || lst_size(lst) != 11)
 		return (EXIT_FAILURE);
 
-	expected_token[0] = (t_token){WORD, "export", 0, TRUE, FALSE};
+	expected_token[0] = (t_token){WORD, "export", 0, TRUE, TRUE};
 	expected_token[1] = (t_token){WORD, "a", 0, TRUE, FALSE};
 	expected_token[2] = (t_token){ASSIGNMENT, "=", 0, FALSE, FALSE};
-	expected_token[3] = (t_token){WORD, "ech", 0, TRUE, FALSE};
+	expected_token[3] = (t_token){WORD, "ech", 0, TRUE, TRUE};
 	expected_token[4] = (t_token){LOGICAL_AND, "&&", 2, FALSE, FALSE};
-	expected_token[5] = (t_token){WORD, "export", 0, TRUE, FALSE};
+	expected_token[5] = (t_token){WORD, "export", 0, TRUE, TRUE};
 	expected_token[6] = (t_token){WORD, "b", 0, TRUE, FALSE};
 	expected_token[7] = (t_token){ASSIGNMENT, "=", 0, FALSE, FALSE};
-	expected_token[8] = (t_token){WORD, "\"o EEEBA\"", 0, TRUE, FALSE};
+	expected_token[8] = (t_token){WORD, "\"o EEEBA\"", 0, TRUE, TRUE};
 	expected_token[9] = (t_token){LOGICAL_AND, "&&", 2, FALSE, FALSE};
 	expected_token[10] = (t_token){WORD, "$a$b", 0, TRUE, FALSE};
 
@@ -158,7 +158,7 @@ char	test_lexer_complex_quoted_with_expandable_and_not_expandable(t_lexer *lexer
 		return (EXIT_FAILURE);
 
 	//"echo ola\"$USER\"seu\" \"pid$$'$USER''hello'''ola\"oi\"\"\"t";
-	expected_token[0] = (t_token){WORD, "echo", 0, TRUE, FALSE};
+	expected_token[0] = (t_token){WORD, "echo", 0, TRUE, TRUE};
 	expected_token[1] = (t_token){WORD, "ola", 0, TRUE, FALSE};
 	expected_token[2] = (t_token){WORD, "\"$USER\"", 0, TRUE, FALSE};
 	expected_token[3] = (t_token){WORD, "seu", 0, TRUE, FALSE};
@@ -195,7 +195,7 @@ char	test_lexer_complex_quote_sequence(t_lexer *lexer)
 		return (EXIT_FAILURE);
 
 	// "echo \"'\"''\"'\"";
-	expected_token[0] = (t_token){WORD, "echo", 0, TRUE, FALSE};
+	expected_token[0] = (t_token){WORD, "echo", 0, TRUE, TRUE};
 	expected_token[1] = (t_token){WORD, "\"'\"", 0, TRUE, FALSE};
 	expected_token[2] = (t_token){WORD, "''", 0, FALSE, FALSE};
 	expected_token[3] = (t_token){WORD, "\"'\"", 0, TRUE, FALSE};
@@ -223,7 +223,7 @@ char	test_lexer_unseparated_quoted_argv(t_lexer *lexer)
 		return (EXIT_FAILURE);
 
 	// "echo \"ola\"'td'\"bem\"";
-	expected_token[0] = (t_token){WORD, "echo", 0, TRUE, FALSE};
+	expected_token[0] = (t_token){WORD, "echo", 0, TRUE, TRUE};
 	expected_token[1] = (t_token){WORD, "\"ola\"", 0, TRUE, FALSE};
 	expected_token[2] = (t_token){WORD, "'td'", 0, FALSE, FALSE};
 	expected_token[3] = (t_token){WORD, "\"bem\"", 0, TRUE, FALSE};
@@ -251,10 +251,49 @@ char	test_lexer_separated_quoted_argv(t_lexer *lexer)
 		return (EXIT_FAILURE);
 
 	// "echo \"ola\"'td'\"bem\"";
-	expected_token[0] = (t_token){WORD, "echo", 0, TRUE, FALSE};
+	expected_token[0] = (t_token){WORD, "echo", 0, TRUE, TRUE};
 	expected_token[1] = (t_token){WORD, "\"ola\"", 0, TRUE, TRUE};
 	expected_token[2] = (t_token){WORD, "'td'", 0, FALSE, TRUE};
 	expected_token[3] = (t_token){WORD, "\"bem\"", 0, TRUE, FALSE};
+
+	int i = -1;
+	while (++i < lst_size(lst))
+	{
+		token = (t_token *)lst->content;
+		// ft_printf("EXPEC_CODE:%d CODE:%d\nEXPEC_STR:%s STR:%s\n\n", expected_token[i].code, token->code, expected_token[i].string, token->string);
+			if (test_helper(expected_token[i], *token))
+				return(EXIT_FAILURE);
+		lst = lst->next;
+	}
+	return (EXIT_SUCCESS);
+}
+
+char	test_lexer_separated_and_unseparated_quoted_argv_with_redir(t_lexer *lexer)
+{
+	t_token	expected_token[100];
+	t_token	*token;
+	t_list *lst = *lexer->token_lst;
+
+	// ft_printf("SIZE: %d\n", lst_size(lst));
+	if (!lst || lst_size(lst) != 15)
+		return (EXIT_FAILURE);
+
+	// "echo oi 'ola' outracoisaai>f1&& echo \"oi\"'ola'\"outracoisaai\" > f2 > f3";
+	expected_token[0] = (t_token){WORD, "echo", 0, TRUE, TRUE};
+	expected_token[1] = (t_token){WORD, "oi", 0, TRUE, TRUE};
+	expected_token[2] = (t_token){WORD, "'ola'", 0, FALSE, TRUE};
+	expected_token[3] = (t_token){WORD, "outracoisaai", 0, TRUE, FALSE};
+	expected_token[4] = (t_token){OUTFILE, ">", 0, FALSE, FALSE};
+	expected_token[5] = (t_token){WORD, "f1", 0, TRUE, FALSE};
+	expected_token[6] = (t_token){LOGICAL_AND, "&&", 2, FALSE, FALSE};
+	expected_token[7] = (t_token){WORD, "echo", 0, TRUE, TRUE};
+	expected_token[8] = (t_token){WORD, "\"oi\"", 0, TRUE, FALSE};
+	expected_token[9] = (t_token){WORD, "'ola'", 0, FALSE, FALSE};
+	expected_token[10] = (t_token){WORD, "\"outracoisaai\"", 0, TRUE, TRUE};
+	expected_token[11] = (t_token){OUTFILE, ">", 0, FALSE, FALSE};
+	expected_token[12] = (t_token){WORD, "f2", 0, TRUE, TRUE};
+	expected_token[13] = (t_token){OUTFILE, ">", 0, FALSE, FALSE};
+	expected_token[14] = (t_token){WORD, "f3", 0, TRUE, FALSE};
 
 	int i = -1;
 	while (++i < lst_size(lst))
@@ -298,6 +337,7 @@ int main(void)
 	tests[5] = "echo \"'\"''\"'\"";
 	tests[6] = "echo \"ola\"'td'\"bem\"";
 	tests[7] = "echo \"ola\" 'td'\" bem\"";
+	tests[8] = "echo oi 'ola' outracoisaai>f1&& echo \"oi\"'ola'\"outracoisaai\" > f2 > f3";
 	// tests[8] = "";
 	tests[10] = "echo \"Hello World";
 
@@ -309,11 +349,12 @@ int main(void)
 	test_functions[5] = test_lexer_complex_quote_sequence;
 	test_functions[6] = test_lexer_unseparated_quoted_argv;
 	test_functions[7] = test_lexer_separated_quoted_argv;
+	test_functions[8] = test_lexer_separated_and_unseparated_quoted_argv_with_redir;
 	// test_functions[8] = test_hell;
 	test_functions[10] = test_lexer_unclosed_quotes;
 
 	int i = -1;
-	while (++i < 8)
+	while (++i < 9)
 	{
 	ft_printf(TEST, i + 1, tests[i]);
 	ft_lexer(&lexer, tests[i]);
