a5cef47dcae7e5d9e97dee6f0000d0a9ea557856
brfialho <brunolegal3@gmail.com>
2026-02-23 02:52:51 -0300

    chore: changed parser_destroy to work with new redir llogic

--- srcs/parsing/parser/parser_destroy.c
+++ srcs/parsing/parser/parser_destroy.c
@@ -6,21 +6,22 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 02:48:34 by brfialho          #+#    #+#             */
-/*   Updated: 2026/02/22 02:56:39 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/02/23 02:44:57 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-static void	del_ast_node(void *content);
+static void	del_ast_content(void *content);
+static void	del_redir_content(void *content);
 
 void	parser_destroy(t_ast **root)
 {
-	ast_del_all(root, del_ast_node);
+	ast_del_all(root, del_ast_content);
 	free(root);
 }
 
-static void	del_ast_node(void *content)
+static void	del_ast_content(void *content)
 {
 	t_msh_ast	*ast;
 	int			i;
@@ -32,8 +33,17 @@ static void	del_ast_node(void *content)
 		while (ast->argv[i])
 			free(ast->argv[i++]);
 		free(ast->argv);
-		lst_del_all(ast->redir, free);
+		lst_del_all(ast->redir, del_redir_content);
 		free(ast->redir);
 	}
 	free(content);
+}
+
+static void	del_redir_content(void *content)
+{
+	t_redir	*redir;
+
+	redir = content;
+	free(redir->target);
+	free(redir);
 }
