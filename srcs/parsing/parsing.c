acd862df82b289243367aad6636ff6fe35db1bff
Bruno <brunolegal3@gmail.com>
2026-02-27 16:10:41 -0300

    wip: heredoc

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,11 +6,53 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 18:22:05 by brfialho          #+#    #+#             */
-/*   Updated: 2026/02/26 22:35:01 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/02/27 16:06:05 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
+
+void	heredoc(t_redir *redir)
+{
+	char	*heredoc_string;
+	char	*line;
+
+	heredoc_string = ft_strdup("");
+	line = readline("> ");
+	while (ft_strncmp(redir->target, line, ft_strlen(line)))
+	{
+		heredoc_string = ft_strjoin_free(heredoc_string , ft_strjoin(line, "\\n"), TRUE, TRUE);
+		free(line);
+		line = readline("> ");
+	}
+	free(line);
+	free(redir->target);
+	redir->target = heredoc_string;
+}
+
+void	collect_heredocs(t_ast *root)
+{
+	t_list	*lst;
+
+	if (root == NULL)
+		return ;
+	if (((t_msh_ast *)root->content)->type == NODE_EXEC)
+	{
+		lst = *((t_msh_ast *)root->content)->redir;
+		while (lst)
+		{
+			if (((t_redir *)lst->content)->type == REDIR_HEREDOC)
+			{
+				ft_printf("END DELIMITER: %s\n", ((t_redir *)lst->content)->target);
+				heredoc(lst->content);
+				ft_printf("FULL DOC: %s\n", ((t_redir *)lst->content)->target);
+			}
+			lst = lst->next;
+		}
+	}
+	collect_heredocs(root->left);
+	collect_heredocs(root->right);
+}
 
 t_bool	parsing(t_mini *mini)
 {
@@ -22,5 +64,6 @@ t_bool	parsing(t_mini *mini)
 	mini->error_code = parser(&mini->root, &mini->lexer);
 	if (mini->error_code)
 		return (parser_error_handler(mini), EXIT_FAILURE);
+	collect_heredocs(*mini->root);
 	return (EXIT_SUCCESS);
 }
