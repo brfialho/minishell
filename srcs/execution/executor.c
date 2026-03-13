cd13a2791d5e67384b561c724420bca2c5e16b98
brfialho <brunolegal3@gmail.com>
2026-03-12 22:29:58 -0300

    fix: status error when cmd not found

--- srcs/execution/executor.c
+++ srcs/execution/executor.c
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 18:24:30 by brfialho          #+#    #+#             */
-/*   Updated: 2026/03/10 23:09:04 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/03/12 22:28:49 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,12 @@ extern int	exec_node(t_ast *node, t_env **env, t_mini *mini);
 
 void	executor(t_mini *mini)
 {
+	char	*value;
+
 	if (!mini || !mini->root)
 		return ;
 	mini->exit_status = exec_node(*mini->root, &mini->env, mini);
+	value = ft_itoa(mini->exit_status);
+	ft_set_env(&mini->env, "?", value);
+	free(value);
 }
