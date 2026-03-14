e893a73e5672889873b013ef3ef823b811095d87
brfialho <brunolegal3@gmail.com>
2026-03-14 04:05:56 -0300

    fix: empty input overwriting error_status

--- srcs/input_pipeline.c
+++ srcs/input_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 19:31:37 by brfialho          #+#    #+#             */
-/*   Updated: 2026/03/14 03:36:07 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/03/14 04:04:15 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 t_bool	process_input_pipeline(t_mini *mini)
 {
 	mini->root = NULL;
-	mini->error_status = NO_ERROR;
 	if (!ft_strcmp(mini->input, WHITESPACE))
 		return (EXIT_FAILURE);
+	mini->error_status = NO_ERROR;
 	mini->error_status = ft_lexer(&mini->lexer, mini->input);
 	if (mini->error_status)
 		return (lexer_error_handler(mini), EXIT_FAILURE);
